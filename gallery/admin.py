6928fd2e3cae8cf45a767878005027341a90145b
morrisonGithinji <morrison.githinji@student.moringaschool.com>
2021-12-02 02:51:28 -0500

    add project files

--- gallery/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Location,Image,Category
-
-
-
-# Register your models here.
-admin.site.register(Location)
-admin.site.register(Image)
-admin.site.register(Category)
